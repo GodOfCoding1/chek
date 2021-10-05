a07f730dd729fd11319a46ed7f6d7ebc1f7b115a
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2021-10-05 12:23:29 +0530

    added scanner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,34 @@
+import {useState,useEffect} from 'react'
+import QrReader from 'react-qr-reader'
 function App() {
-  return <>hi</>;
+  const [scanData, setScanData] = useState([]);
+  const [flag, setFlag] = useState(false);
+  const handleError = (e)=>{
+  }
+  const handleScan = (data)=>{
+    if(flag && data){
+      setFlag(false);
+      setScanData([...scanData,data]);
+    }
+  }
+  useEffect(() => {
+    console.log(scanData)
+    return () => {
+      
+    }
+  }, [scanData])
+  return(
+    <div>
+        {flag?<QrReader
+          delay={100}
+          onError={handleError}
+          onScan={handleScan}
+          style={{ width: '50%' }}
+        />:<button onClick={()=>{setFlag(true)}}>Scan</button>}
+      </div>
+  )
+  
+  
 }
 
 export default App;
